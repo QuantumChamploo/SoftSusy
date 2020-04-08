9a9618e2297c43440ec8e4c17f41e5f104146d1d
Neil Leonard <quantumchamploo@gmail.com>
2020-04-07 20:28:59 -0500

    made plots for lisa

--- SlhaPlotLib/plottesting2.py
+++ SlhaPlotLib/plottesting2.py
@@ -1,14 +1,14 @@
 from graphObs import *
 
-d = Graph("sunday.slha",[25])
-#d = Graph("test.slha")
+#d = Graph("sunday.slha",[25])
+d = Graph("slha/span5Point3.slha")
 
 
-#d.plot()
+d.plot()
 #d.plot(includes=['squarks','sleptons'])
-d.plotBar()
+#d.plotBar()
 
-plt.ylim(-500,11000)
-plt.title("This is a pretty great title")
+plt.ylim(-500,15000)
+plt.title("mMess  10^14 Lambda 10^5.77 TanBeta 20")
 
 plt.show()
