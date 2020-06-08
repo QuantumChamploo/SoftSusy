dae566f26c705556e04504b6d417a2373d31e526
Neil Leonard <quantumchamploo@gmail.com>
2020-06-08 13:10:41 -0500

    addded some notes to the doc

--- SlhaPlotLib/plottesting2.py
+++ SlhaPlotLib/plottesting2.py
@@ -1,5 +1,8 @@
 from graphObs import *
+import time
 
+
+start = time.time()
 #d = Graph("sunday.slha",[25])
 d = Graph("slha/span5Point3.slha")
 
@@ -11,4 +14,8 @@ d.plot()
 plt.ylim(-500,15000)
 plt.title("mMess  10^14 Lambda 10^5.77 TanBeta 20")
 
+end = time.time()
+print(end - start)
+
 plt.show()
+
